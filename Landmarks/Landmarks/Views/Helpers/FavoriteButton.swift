7c7721e9d6eb8b80e9c79acdd48707f8d45a8c23
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-03-12 18:22:32 +0900

    add: ディレクトリ構成を修正し、FavoriteButton を Detail に追加

---
+++ Landmarks/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Daichi Hayashi on 2021/03/12.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }, label: {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        })
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
