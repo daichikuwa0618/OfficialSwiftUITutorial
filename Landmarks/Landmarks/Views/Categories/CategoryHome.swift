75a2b98174d5ea1a9c44f04632565ab78a7bbab9
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-03-15 01:27:40 +0900

    add: CategoryHome の雛形を追加

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Daichi Hayashi on 2021/03/15.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
