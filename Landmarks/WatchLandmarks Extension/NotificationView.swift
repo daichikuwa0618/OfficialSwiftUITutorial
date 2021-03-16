7f7a3f185c83ade27f800c93401a14a8f336cc95
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-03-16 19:04:56 +0900

    add: 通知機能

--- Landmarks/WatchLandmarks Extension/NotificationView.swift
+++ Landmarks/WatchLandmarks Extension/NotificationView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+
+            Divider()
+
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        Group {
+            NotificationView()
+            NotificationView(title: "Turtle Rock",
+                             message: "You are within 5 miles of Turtle Rock.",
+                             landmark: ModelData().landmarks[0])
+        }
     }
 }
