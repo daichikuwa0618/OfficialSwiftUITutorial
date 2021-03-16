164cc238fd1ffbe62a5ee03dd31c0bfca03c970e
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-03-16 19:45:46 +0900

    add: ToolBar を追加

--- Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,11 +10,27 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        var id: FilterCategory { self }
+    }
 
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+                && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
 
     var body: some View {
@@ -30,8 +46,28 @@ struct LandmarkList: View {
                     }
                 }
             }
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
             .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(InlinePickerStyle())
+
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorite only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+
+            Text("Slect a Landmark")
         }
     }
 }
